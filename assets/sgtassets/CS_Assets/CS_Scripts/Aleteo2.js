a3eab326f75a834980112bb160a6ccecf93ac251
Manuel Alejandro Daza <manuel-d10@hotmail.com>
2016-10-06 23:16:20 -0500

    chevignon
    Inicio de repositorio

---
+++ assets/sgtassets/CS_Assets/CS_Scripts/Aleteo2.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var frames : Sprite[];
+var framesPerSecond = 10.0;
+
+private var timer = 0.0;
+
+
+function Start () {
+
+}
+
+function Update () {
+	timer = (timer + Time.deltaTime * framesPerSecond) % frames.Length;
+
+	GetComponent(SpriteRenderer).sprite = frames[timer];
+}
